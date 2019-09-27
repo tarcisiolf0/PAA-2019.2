1568e08a2dbee7c6e919f2d850d3cb63234a6691
Tarcísio Lima <tlf@ic.ufal.br>
2019-09-27 10:35:27 -0300

    24-09-19
    Adicionado os códigos da aula ministrada no dia 24/09/19

---
+++ Aula_24_09_19/soma_máxima_bruta.py
@@ -0,0 +1,23 @@
+
+# Soma máxima bruta
+while True:
+    try:
+        n = int(input())
+    except (EOFError):
+        break
+
+    inputList = []
+    for i in range(0, n):
+        inputList.append(int(input()))
+
+    maxSum = 0
+    for i in range(0, len(inputList)):
+        for j in range(i, len(inputList)):
+            sum = 0
+            for k in range(i, j):
+                sum += inputList[k]
+            if sum > maxSum:
+                maxSum = sum
+
+    print("\n")
+    print(maxSum)
